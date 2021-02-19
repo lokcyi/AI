5b021bb2b96fad454fe1dfbb618840b72a113b8a
vanessafan <vanessafan@powerchip.com>
2021-02-19 20:16:32 +0800

    GA

--- 5.邏輯斯迴歸模型_LogisticRegression.py
+++ 5.邏輯斯迴歸模型_LogisticRegression.py
@@ -29,14 +29,14 @@ w0=float(model.intercept_) #intercept 級距
 def sigmoid(x,wo,w1):
     ln_odds=wo+w1*x
     return 1/(1+np.exp(-ln_odds))
-#===
+#========================
 
 x=np.arange(0,10,1)
 s_x=sigmoid(x,w0,w1)
 plt.plot(x,s_x)
 plt.axhline(y=0.5, ls='dotted', color='k')
 plt.show()
-#===#===#===#===#===
+#=========================================================
 print('result',model.predict([[0],[1],[2],[3]])) #預測每天喝 0 1 2 3 是否會得到糖尿病 ==>result [0 0 1 1]
 model.predict_proba(X) #可以看分析出來 得到的機率 ，因而決定是否會得到。
 # array([[0.68862321, 0.31137679], #得到糖尿病的機率 ,不會得到糖尿病的機率 ==> 何者大 決定不會不得糖尿病

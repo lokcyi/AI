c8f25107d77033a650e78511ce236478efd4e7ad
vanessa <lokcyi@gmail.com>
2021-04-13 00:08:15 +0800

    sdaddasdsa

--- MLFramework.v0.01/MLSample_SCM_MegeQuarter.py
+++ MLFramework.v0.01/MLSample_SCM_MegeQuarter.py
@@ -1,3 +1,4 @@
+from os import replace
 import pandas as pd
 import numpy as np 
 from BaseClass.MLBase import MLBase ,fillNaType
@@ -23,13 +24,22 @@ class MLSample(MLBase):
         self.config.includeColumns = []
         self.config.excludeColumns =['PM','TS','ENG','NST','STOCK_EVENT_TIME','TOOL_ID']
         self.config.fillNaType=fillNaType.MEAN
-        self.config.modelFileKey="Parts_Tools_30Quater" 
+        self.config.modelFileKey="Parts_Tools_30Quater_85-EMA0130" 
         self.config.forceRetrain=True
          
         # self.config.runModel=['DNN','DNN1k','LRModel','NN','RFModel','XG']
-        #self.config.runModel=['LRModel']#,'NN','CAT']
-
-        self.config.runModel=['CAT']
+        self.config.runModel=['LRModel','NN','CAT']
+        self.partno='85-EMA0130'
+# 85-EMA0920
+# 85-EMA0130
+# 85-EMA0910 怪
+# 85-ECT0010 good
+# 85-EMA0900 NG
+# 86-DIA0120 OK
+# 87-WPT1070 soso
+# 85-EKA0270 not bad
+# 85-EKA0190 goodß
+        # self.config.runModel=['CAT']
         #self.scaler
         #self.scalerColumnList=[]
         self.dataPreHandler()
@@ -37,17 +47,19 @@ class MLSample(MLBase):
     def dataPreHandler(self):
         df_parts=pd.read_csv("./data/Parts_EQP_Output_ByMonth_20210407_van.csv")
         df_parts['MFG_MONTH'] = pd.to_datetime(df_parts['STOCK_EVENT_TIME'].values, format='%Y-%m-%d').astype('period[Q]')
-        df_parts.groupby(['PART_NO','EQP_NO','MFG_MONTH']).sum().reset_index()
+        df_parts.drop(columns=['STOCK_EVENT_TIME'],inplace=True)
+        df_parts = df_parts.groupby(['PART_NO','EQP_NO','MFG_MONTH']).sum().reset_index()
         df_EQP=pd.read_csv("./data/ScmTrainingData_Monthly_30_20152021.csv")
         df_EQP['MFG_MONTH'] = pd.to_datetime(df_EQP['MFG_MONTH'].values, format='%Y%m').astype('period[Q]')
-        df_EQP.groupby(['TOOL_ID','MFG_MONTH']).mean().reset_index()
-        pd.merge(df_parts, df_EQP, left_on=['EQP_NO','MFG_MONTH'], right_on=['TOOL_ID','MFG_MONTH'],how="inner").to_csv(self.config.datafilee, index=False)
+        df_EQP = df_EQP.groupby(['TOOL_ID','MFG_MONTH']).mean().reset_index()
+        df_merge = pd.merge(df_parts, df_EQP, left_on=['EQP_NO','MFG_MONTH'], right_on=['TOOL_ID','MFG_MONTH'],how="inner")
+        df_merge.to_csv(self.config.datafile, index=False)
 
         
 
     def dataTransform(self):
         self.dfInputData['MFG_MONTH'] = self.dfInputData['MFG_MONTH'].astype(str)   
-        self.dfInputData = self.dfInputData[self.dfInputData['PART_NO']=='86-DIA0120']  
+        self.dfInputData = self.dfInputData[self.dfInputData['PART_NO']==self.partno]  
 
     ##填補遺漏值##
     def fillnull(self):
